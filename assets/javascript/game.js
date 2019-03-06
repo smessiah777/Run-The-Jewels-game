a4899cdf3341d96eac7b65032e9e028118b4874d
roninTDK <smessiah17@gmail.com>
2019-03-05 22:06:11 -0600

    completed game fucntionality. assignment is complete but would like to add a few more style aspects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,22 +20,87 @@ $(document).ready(function() {
 
   var wins = 0;
   var losses = 0;
-  var jewelScore = 0;
-  var targetScore = 0;
 
   //hero section button start
   $("#enter-icon").on("click", function() {
-    gameNum();
+    start();
   });
 
-  function gameNum() {
-    var goalNum = Math.round(Math.random() * (120 - 19) + 19);
-    $("#game-number").html(goalNum);
-    console.log("I am the goal number " + goalNum);
-  }
+  //random number generation and adding the value to the necessary elements
+  var rndNum = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-  //   function jewelNum() {
-  //     var rndJewel = Math.round(Math.random() * (12 - 1) + 1);
-  //     $(".jewel-img").html(rndJewel);
-  //   }
+  var start = function() {
+    sumScore = 0;
+    gameNum = rndNum(19, 120);
+    jewels.j1.value = rndNum(1, 12);
+    jewels.j2.value = rndNum(1, 12);
+    jewels.j3.value = rndNum(1, 12);
+    jewels.j4.value = rndNum(1, 12);
+
+    $("#game-number").text(gameNum);
+
+    console.log("j1 ", jewels.j1.value);
+    console.log("j2 ", jewels.j2.value);
+    console.log("j3 ", jewels.j3.value);
+    console.log("j4 ", jewels.j4.value);
+    console.log("random game number ", gameNum);
+  };
+
+  //Win conditions and logic
+  var victory = function() {
+    //matches added jewel score to game number
+    if (sumScore > gameNum) {
+      console.log("You Lose");
+      losses++;
+      $("#losses").text(losses);
+      roundReset();
+    } else if (sumScore === gameNum) {
+      console.log("You Win");
+      // alert("won");
+      wins++;
+      $("#wins").text(wins);
+      roundReset();
+    }
+  };
+  ///adds current player score to value of new jewel clicked
+  var addJewels = function(runJewel) {
+    sumScore += runJewel.value;
+    $("#score-number").text(sumScore);
+    victory();
+
+    console.log("I am the player score ", sumScore);
+  };
+  //hard reset game, generate new game number and clear player
+  var resetHard = function() {
+    start();
+    $(sumScore).empty();
+    $("#score-number").html("0");
+    $(wins).empty();
+    $(losses).empty();
+    $("#wins").html("0");
+    $("#losses").html("0");
+  };
+  //clear player score from last round and generate new game number
+  var roundReset = function() {
+    start();
+    $(sumScore).empty();
+    $("#score-number").html("0");
+  };
+
+  $("#j1").click(function() {
+    addJewels(jewels.j1);
+  });
+  $("#j2").click(function() {
+    addJewels(jewels.j2);
+  });
+  $("#j3").click(function() {
+    addJewels(jewels.j3);
+  });
+  $("#j4").click(function() {
+    addJewels(jewels.j4);
+  });
+
+  $("#reset-btn").click(resetHard);
 });
